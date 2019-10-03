d6704273b616f7042cfea6bad2151b0bd66f4b7b
xyc309 <xyc309@gmail.com>
2019-10-04 07:32:03 +0800

    add Test

---
+++ TicTacToe.Services/GameWinnerService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe.Services
+{
+    public interface IGameWinnerService
+    {
+        char Validate(char[,] gameBoard);
+    }
+
+    public class GameWinnerService : IGameWinnerService
+    {
+        private const char SymbolForNoWinner = ' ';
+
+        public char Validate(char[,] gameBoard)
+        {
+            var currentWinningSymbol = CheckForThreeInARowInHorizontalRow(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowInVerticalColumn(gameBoard);
+            if (currentWinningSymbol != SymbolForNoWinner)
+                return currentWinningSymbol;
+            currentWinningSymbol = CheckForThreeInARowDiagonally(gameBoard);
+            return currentWinningSymbol;
+        }
+
+        private static char CheckForThreeInARowInVerticalColumn(char[,] gameBoard)
+        {
+            var rowOneChar = gameBoard[0, 0];
+            var rowTwoChar = gameBoard[1, 0];
+            var rowThreeChar = gameBoard[2, 0];
+            if (rowOneChar == rowTwoChar &&
+                rowTwoChar == rowThreeChar)
+            {
+                return rowOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+
+        private static char CheckForThreeInARowInHorizontalRow(char[,] gameBoard)
+        {
+            var columnOneChar = gameBoard[0, 0];
+            var columnTwoChar = gameBoard[0, 1];
+            var columnThreeChar = gameBoard[0, 2];
+            if (columnOneChar == columnTwoChar &&
+                columnTwoChar == columnThreeChar)
+            {
+                return columnOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+
+        private static char CheckForThreeInARowDiagonally(char[,] gameBoard)
+        {
+            var cellOneChar = gameBoard[0, 0];
+            var cellTwoChar = gameBoard[1, 1];
+            var cellThreeChar = gameBoard[2, 2];
+            if (cellOneChar == cellTwoChar &&
+                cellTwoChar == cellThreeChar)
+            {
+                return cellOneChar;
+            }
+            return SymbolForNoWinner;
+        }
+    }
+}
---
+++ TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using TicTacToe.Services;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    public class GameWinnerServiceTests
+    {
+        private IGameWinnerService _gameWinnerService;
+        private char[,] _gameBoard;
+
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            _gameWinnerService = new GameWinnerService();
+            _gameBoard = new char[3, 3]
+                  {
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '}
+                  };
+        }
+        [Test]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PlayerWithAllSpacesInTopRowIsWinner()
+        {
+            const char expected = 'X';
+            for (var rowIndex = 0; rowIndex < 3; rowIndex++)
+            {
+                _gameBoard[0, rowIndex] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [Test]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 0] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [Test]
+        public void PlayerWithThreeInARowDiagonallyDownAndToRightIsWinner()
+        {
+            const char expected = 'X';
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
