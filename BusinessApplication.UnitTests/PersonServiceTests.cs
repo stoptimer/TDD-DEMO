8c04e789e1abaae62e0175f3e49e91e0eab574bb
xyc309 <xyc309@gmail.com>
2019-10-07 07:45:22 +0800

    edit persontest

--- BusinessApplication.UnitTests/PersonServiceTests.cs
+++ BusinessApplication.UnitTests/PersonServiceTests.cs
@@ -16,8 +16,17 @@ namespace BusinessApplication.UnitTests
         public void SetUp()
         {
             _personRepository = new Mock<IPersonRepository>();
-            _personRepository.Setup(x=>x.GetPerson())
-            _personService = new PersonService(_personRepository);
+            _personRepository.Setup(x=>x.GetPerson(It.IsAny<int>())).Returns(new Person() { Id = 1, FirstName = "John", LastName = "Doa" });
+            _personService = new PersonService(_personRepository.Object);
+        }
+        [Test]
+        public void ShouldBeAbleToCallPersonServiceAndGetPerson()
+        {
+            var expected = new Person() { Id=1,FirstName="John",LastName="Doa"};
+            var actual = _personService.GetPerson(expected.Id);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
         }
     }
 }
